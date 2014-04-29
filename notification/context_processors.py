d7c4f0471271d104c0ff3500033e425547ca6c27
Graham Kaplan <graham.kaplan@gmail.com>
2014-04-29 15:40:55 -0700

    Add user notifications to context processor

--- notification/context_processors.py
+++ notification/context_processors.py
@@ -5,6 +5,7 @@ def notification(request):
     if request.user.is_authenticated():
         return {
             "notice_unseen_count": Notice.objects.unseen_count_for(request.user, on_site=True),
+            "notifications": Notice.objects.filter(user=request.user.id)
         }
     else:
         return {}
