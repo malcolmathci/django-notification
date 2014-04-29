ccccec0e42b9769512e30f1a371f40fc4a703605
Graham Kaplan <graham.kaplan@gmail.com>
2014-04-29 15:48:09 -0700

    lots of great commits today

--- notification/context_processors.py
+++ notification/context_processors.py
@@ -5,7 +5,7 @@ def notification(request):
     if request.user.is_authenticated():
         return {
             "notice_unseen_count": Notice.objects.unseen_count_for(request.user, on_site=True),
-            "notifications": Notice.objects.filter(user=request.user.id)
+            "notifications": Notice.objects.filter(recipient=request.user.id)
         }
     else:
         return {}
