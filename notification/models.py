e34fff336475d83f922edc025ca8272fa62cb767
Mohammed Affan <mohammed@thebimhub.com>
2014-09-03 18:38:01 +0400

    Revert "modified notice model to handle companies via generic foreign keys"
    This reverts commit f10f1f5e8bbe4f25ec51ff90f0785569064977c9.

--- notification/models.py
+++ notification/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 from __future__ import print_function
 
 import base64
+import datetime
 
 from django.db import models
 from django.db.models.query import QuerySet
@@ -11,10 +12,8 @@ from django.utils.translation import ugettext_lazy as _
 from django.utils.translation import get_language, activate
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.six.moves import cPickle as pickle  # pylint: disable-msg=F
+from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
-from django.contrib.contenttypes.models import ContentType
-from django.contrib.contenttypes import generic
-
 
 from .compat import AUTH_USER_MODEL
 
@@ -171,11 +170,7 @@ class NoticeManager(models.Manager):
 class Notice(models.Model):
 
     recipient = models.ForeignKey(AUTH_USER_MODEL, related_name="recieved_notices", verbose_name=_("recipient"))
-
-    content_type = models.ForeignKey(ContentType)
-    object_id = models.PositiveIntegerField()
-    sender = generic.GenericForeignKey('content_type', 'object_id')
-
+    sender = models.ForeignKey(AUTH_USER_MODEL, null=True, related_name="sent_notices", verbose_name=_("sender"))
     message = models.TextField(_("message"))
     notice_type = models.ForeignKey(NoticeType, verbose_name=_("notice type"))
     added = models.DateTimeField(_("added"), auto_now_add=True, editable=False)
